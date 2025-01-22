b3e3dd12c50dc5a52358d03872dd3556cf4061a5
njeeevan2216 <147393797+njeeevan2216@users.noreply.github.com>
2025-01-22 11:36:26 +0530

    I had a doubt in SCOPE

---
+++ doubt/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int i = 47;
+
+int main() {
+    int i = 45;
+    for(int i = 0; i<1 ; i++){
+        cout << i << endl;
+    };
+    cout << i << endl;
+}
